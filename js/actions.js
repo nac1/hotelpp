860cc7304f3f78565fdabde6a5acac5613a7f6ba
nac1 <ignacio.h.velasco@gmail.com>
2014-12-06 12:13:47 -0600

    config y js
    se agrego config y js para api de telefono

---
+++ js/actions.js
@@ -0,0 +1,58 @@
+/*function listo()
+{
+    alert('listo');
+}
+
+
+document.addEventListener('click',listo,false);
+*/
+
+
+//objeto de tipo estandar
+
+/*
+function init()
+{ alert(tn.otro);
+}*/
+
+
+var fn= {
+//agregar propiedades y metodos
+    ready:function(){
+    
+    document.addEventListener('deviceready',fn.init,false);
+ 
+
+},
+    
+
+   init:function(){
+        //alert(fn.otro);
+        $('#regSend').click(fn.enviaRegistro);
+       var x=false;
+       if(!x)   
+       window.location.href='#reg'; //con esto forzamos a que siempre se registre la personae
+       
+    },
+        
+    enviaRegistro:function(){
+        var nom=$('#regNom').val();
+        var tel=$('#regTel').val();
+        var mai=$('#regMail').val();
+        
+        if(nom !='' && tel != '' && mail !='')
+           {
+         navigator.notification.alert('correcto',null,'felicidades','aceptar');
+           }else{
+         alert('Todos los campos son requeridos');
+            }
+  
+        }
+    
+};
+
+$(fn.ready);
+            
+//window.addEventListener('load',init,false);
+//jQuery(document).ready(init); //alias es el $ en lugar de jquery
+//$(document).ready(init);
